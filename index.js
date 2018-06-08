62ec3326ecb0f7de71ae70cec7dc8599b9dac1a4
Derrick Skipwith <dskipwith15@gmail.com>
2018-06-08 16:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
-+function add(3,5){
-+  return 3 + 5;
-+}
-+
-+function subtract(a,b){
-+  return a - b;
-+}
-+
-+function multiply(a,b){
-+  return a * b;
-+}
-+
-+function divide(a,b){
-+  return a / b;
-+}
-+
-+function inc(n){
-+  return n += 1;
-+}
-+
-+function dec(n){
-+  return n -= 1;
-+}
-+
-+function makeInt(n){
-+  return parseInt(n, 10);
-+}
-+
-+function preserveDecimal(n){
-+  return parseFloat(n);
-+}
+function add(3,5){
+  return 3 + 5;
+}
+
+function subtract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function inc(n){
+  return n += 1;
+}
+
+function dec(n){
+  return n -= 1;
+}
+
+function makeInt(n){
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
